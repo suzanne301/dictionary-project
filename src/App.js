5aa3714779c4687715acc0ae0c37eed99e761f97
suzanne301 <104364608+suzanne301@users.noreply.github.com>
2024-07-20 16:41:36 +0100

    created a search bar and linked it with event handler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import logo from "./logo.jpg"
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <img 
-      src={logo}
-      alt='logo'
-      className="img-fluid App-logo" 
-      />
-      </header>
+      <div className="container">
+        <header className="App-header">
+        <img 
+        src={logo}
+        alt='logo'
+        className="img-fluid App-logo" 
+        />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+           <small>
+            This project was coded by {""}
+            <a href="https://www.linkedin.com/in/suzanne-paterson-778b492b9/" target="_blank">Suzanne Paterson</a>, is open-sourced on {""} 
+            <a href="https://github.com/suzanne301/dictionary-project" target="_blank">GitHub</a> and hosted on {""}
+            <a href="https://dictionary-paterson.netlify.app/" target="_blank">Netlify</a>.
+            </small>
+        </footer>
+      </div>
     </div>
   );
 }
