a63ea471d4cda7f519945ed08d3a72b77120472b
suzanne301 <104364608+suzanne301@users.noreply.github.com>
2024-08-11 22:46:20 +0100

    styled the application

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
           />
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="agile" />
         </main>
         <footer className="App-footer">
            <small>
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Meaning.css";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
@@ -9,14 +10,15 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              <strong>Definition: </strong>
+            <div className="definition">
               {definition.definition}
-              <br />
-              <strong>Example: </strong>
-              <em>{definition.example}</em>
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
+            </div>
+
+            <div className="example">
+              {definition.example}
+            </div>
+            
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
